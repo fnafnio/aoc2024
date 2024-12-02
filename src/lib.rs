926952830aba4a931ed6dd7eeff4c2dea2de393e
Fabio Pungg <fabio.pungg@medel.com>
2024-12-02 08:30:32 +0100

    build fixed

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod solutions;
 
 // should be done with macros or something
 const SOLVERS: &[&dyn Solver] = &[
-    &Day1, &Day2, &Day3, &Day4, &Day5, &Day6, &Day7, &Day8, &Day9, &Day10, &Day11, &Day12, &Day13,
+    &Day01, &Day02, &Day03, &Day04, &Day05, &Day06, &Day07, &Day08, &Day09, &Day10, &Day11, &Day12, &Day13,
     &Day14, &Day15, &Day16, &Day17, &Day18, &Day19, &Day20, &Day21, &Day22, &Day23, &Day24, &Day25,
 ];
 
