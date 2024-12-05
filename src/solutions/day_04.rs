779dda9bf370598026f91b089f09f3ab12d04d0f
Fabio Pungg <fabiopungg@gmail.com>
2024-12-05 06:22:36 +0100

    pause here

--- src/solutions/day_04.rs
+++ src/solutions/day_04.rs
@@ -1,3 +1,5 @@
+use std::cmp::Ordering;
+
 use crate::Solver;
 use color_eyre::eyre::{eyre, Result};
 
@@ -20,13 +22,66 @@ fn solve_2(input: &str) -> Result<usize> {
     Err(eyre!("not yet implemented"))
 }
 
+#[derive(Debug)]
+struct Board {
+    board: String,
+    width: usize,
+    height: usize,
+}
+
+const XMAS: &str = "XMAS";
+impl Board {
+    fn new(board: &str) -> Self {
+        let width = board.lines().next().unwrap().len();
+        let height = board.lines().count();
+        let board: String = board.chars().filter(|c| *c != '\n' && *c != '\r').collect();
+        Self {
+            board,
+            width,
+            height,
+        }
+    }
+
+    fn find_all_x(&self) -> Vec<usize> {
+        self.board
+            .char_indices()
+            .filter_map(|(i, c)| if c == 'X' { Some(i) } else { None })
+            .collect()
+    }
+
+    fn forward(&self, start: usize) -> bool {
+        self.board[start..start + XMAS.len()].cmp(XMAS) == Ordering::Equal
+    }
+
+    fn backward(&self, start: usize) -> bool {
+        // start >= XMAS.len() &&
+        self.board[start-XMAS.len()..=start].chars().rev()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use assert_ok::assert_ok;
-    const INPUT: &str = "";
-    const SOLUTION_1: usize = 0;
+    const INPUT: &str = "
+..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....";
+    const SOLUTION_1: usize = 18;
     const SOLUTION_2: usize = 0;
+
+    #[test]
+    fn test_build_1() {
+        let b = dbg!(Board::new(INPUT));
+        let v = b.find_all_x();
+        assert_eq!(v, vec![2, 10, 18, 25]);
+        let f: Vec<_> = v.iter().map(|i| b.forward(*i)).collect();
+        assert_eq!(f, vec![false, false, true, false]);
+        let f: Vec<_> = v.iter().map(|i| b.backward(*i)).collect();
+        assert_eq!(f, vec![false, true, false, false]);
+    }
 
     #[test]
     fn test_1() {
